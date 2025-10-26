4f2a7a032c2e64ae30e010bc867d7fb54ceb99dc
Thokalaharish8872 <159756302+Thokalaharish8872@users.noreply.github.com>
2025-10-27 01:24:08 +0530

    Time: 0 ms (100%), Space: 45.4 MB (21.6%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -5,32 +5,44 @@ class Solution {
         int[] color = new int[n];
 
         for(int i=0;i<n;i++){
-            ArrayList<Integer> a = new ArrayList<>();
-            for(int ele : graph[i]){
-                a.add(ele);
-            }
-            al.add(a);
-        }
-
-        for(int i=0;i<n;i++){
             if(color[i] == 0){
-                if(!bfs(al,color,i)) return false;
+                if(!dfs(graph,color,i)) return false;
             }
         }
         return true;
 
     }
 
-    private boolean bfs(ArrayList<ArrayList<Integer>> al,int[] color,int i){
+    private boolean dfs(int[][] graph,int[] color,int i){
+        if(color[i] == 0) color[i] = 1;
+
+        for(int neig : graph[i]){
+            if(color[neig] == 0){
+                if(color[i] == 1) color[neig] = 2;
+                else color[neig] = 1;
+                
+                if(!dfs(graph,color,neig)) return false;
+            }
+            else{
+                if(color[neig] == color[i]) return false;
+            }
+
+        }
+        return true;
+    }
+
+    private boolean bfs(int[][] graph,int[] color,int i){
         Queue<Integer> q = new LinkedList<>();
         q.add(i);
         color[i] = 1;
 
         while(!q.isEmpty()){
             int curr = q.remove();
-            for(int neig : al.get(curr)){
+
+            for(int neig : graph[curr]){
                 if(color[neig] == 0){
                     q.add(neig);
+
                     if(color[curr] == 1) color[neig] = 2;
                     else color[neig] = 1;
                 }
